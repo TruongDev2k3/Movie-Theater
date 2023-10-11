cd03cc3ea2fd95b5b6ce18f18754cdcad47a233c
TruongDev2k3 <nguyenvantruong05032003@gmail.com>
2023-10-11 22:47:53 +0700

    Update customer controller

---
+++ BTL_NguyenVanTruong_/API_User/Controllers/CustomersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_NguyenVanTruong_.API_User.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+    }
+}
